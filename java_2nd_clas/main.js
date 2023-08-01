39ecaf91670d0b03bc78f2fbf2d58feb1b1d746e
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-08-01 12:55:30 +0600

    Add files via upload

---
+++ java_2nd_clas/main.js
@@ -0,0 +1,16 @@
+var change =document.querySelector("#demo")
+change.innerText=("Hello")
+
+var list=document.querySelectorAll('ul li');
+
+for(let i=0; i<=list.length ;i++){
+    var all = list[i]
+    all.innerText="tushar"
+
+
+    
+}
+
+
+
+
