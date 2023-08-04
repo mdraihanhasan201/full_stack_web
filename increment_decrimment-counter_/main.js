b88a24392851ce8fc48d024f1878dee54da22941
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-08-04 23:16:56 +0600

    Add files via upload
    update

--- increment_decrimment-counter_/main.js
+++ increment_decrimment-counter_/main.js
@@ -1,12 +1,72 @@
-var incriment =document.getElementById("button");
-con=0;
-var counter =document.getElementById("counter")
+let button = document.getElementById('button')
+let button2 = document.getElementById('button2')
+let count = document.getElementById("counter");
+let ulElement = document.getElementById('list-item');
 
-var utsho=()=>{
-    let num=con++;
-    counter.innerHTML=num;
+
+let counter=0;
+function increment (){
+    counter++
+    count.innerHTML=counter;
+    let li = document.createElement('li');
+    ulElement.appendChild(li);
+    let text = document.createTextNode(counter+'sentance');
+    li.appendChild(text);
+    li.innerHTML="<i> karim<i>"+counter;
+    li.setAttribute=('data-counter'+counter);
+    console.log(li);
+
+
+
+   
+}
+
+function decrement (){
+    let li =ulElement.querySelector('[data-counter="'+counter+'" ]');
+    li.remove()
+
+    counter--
+    count.innerHTML=counter;
+    
+    
+    
+
+    
 
 }
 
+button.addEventListener('click',increment);
+button2.addEventListener('click',decrement);
 
-incriment.addEventListener('click',utsho)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var incriment =document.getElementById("button");
+// con=0;
+// var counter =document.getElementById("counter")
+
+// var utsho=()=>{
+//     let num=con++;
+//     counter.innerHTML=num;
+
+// }
+
+
+// incriment.addEventListener('click',utsho)
